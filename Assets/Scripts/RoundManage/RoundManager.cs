875abcce8256019ee60441518057db0f8540d869
clément bostyn <clemclem20002@gmail.com>
2023-09-27 12:48:47 +0200

    round manager et team up finis sur papier

---
+++ Assets/Scripts/RoundManage/RoundManager.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    private enum RoundState
+    {
+        SoloRound,
+        DuoRound,
+        TrioRound
+    }
+    [SerializeField]
+    private int _roundBeforeTeamRound = 3;
+    [SerializeField]
+    private RoundState _roundState = RoundState.SoloRound;
+    [SerializeField]
+    private bool _isRoundRunning = false;
+    [SerializeField]
+    private int _roundNumber = 0;
+    [SerializeField]
+    private int _aliveCount = 0;
+    [SerializeField]
+    private bool _isRoundFinished = false;
+    [SerializeField]
+    private bool _isRoundStarted = false;
+    [SerializeField]
+    private bool _isTeam1Dead = false;
+    [SerializeField]
+    private bool _isTeam2Dead = false;
+    [SerializeField]
+    private GameObject[] _players;
+    [SerializeField]
+    private List<GameObject> _team1, _team2, _playerAvailable;
+
+    private Timer _timer;
+
+    public bool _isPlayer1Dead = false;
+    public bool _isPlayer2Dead = false;
+    public bool _isPlayer3Dead = false;
+    public bool _isPlayer4Dead = false;
+
+
+    private void Start()
+    {
+        _timer = FindAnyObjectByType<Timer>();
+    }
+    public void checkWin()
+    {
+        if(_roundState == RoundState.SoloRound) // 1v1v1v1
+        {
+            if (_aliveCount == 1)
+            {
+                newRoud();
+                _timer.StopTimer();
+            }
+        }
+        else if(_roundState == RoundState.DuoRound) // 2v2
+        {
+            if (_isTeam1Dead)
+            {
+                newRoud();
+                _timer.StopTimer();
+
+            }
+            else if (_isTeam2Dead)
+            {
+                newRoud();
+                _timer.StopTimer();
+
+            }
+        }
+        else if(_roundState == RoundState.TrioRound) //1v3
+        {
+            if (_isTeam1Dead) //team 1 toujours solo
+            {
+                newRoud();
+                _timer.StopTimer();
+
+            }
+            else if (_isTeam2Dead) //team 2 toujours trio
+            {
+                newRoud();
+                _timer.StopTimer();
+
+            }
+        }
+    }
+
+    public void newRoud()
+    {
+        _roundNumber++;
+        _isRoundFinished = false;
+        _isRoundStarted = false;
+        _isPlayer1Dead = false;
+        _isPlayer2Dead = false;
+        _isPlayer3Dead = false;
+        _isPlayer4Dead = false;
+        _isTeam1Dead = false;
+        _isTeam2Dead = false;
+        _aliveCount = 0;
+        _playerAvailable.Clear();
+        _team1.Clear();
+        _team2.Clear();
+
+        _playerAvailable = new List<GameObject>(_players);
+
+        if(_roundNumber >= _roundBeforeTeamRound)
+            choiceTypeRound();
+
+        _timer.StartTimer();
+
+
+    }
+
+    public void choiceTypeRound()
+    {
+        int random = Random.Range(0, 3);
+        if (random == 0)
+        {
+            _roundState = RoundState.SoloRound;
+            foreach (GameObject player in _players)
+            {
+                player.tag = "player";
+            }
+        }
+        else if (random == 1)
+        {
+            _roundState = RoundState.DuoRound;
+            for (int i = 1; i <= 4; i++) // 2 joueurs par équipe
+            {
+                int randomIndex = Random.Range(0, _playerAvailable.Count);
+                if (i <= 2)
+                {
+                    _playerAvailable[randomIndex].tag = "team1";
+                    _team1.Add(_playerAvailable[randomIndex]);
+                    _playerAvailable.RemoveAt(randomIndex);
+                }
+                else
+                {
+                    _playerAvailable[randomIndex].tag = "team2";
+                    _team2.Add(_playerAvailable[randomIndex]);
+                    _playerAvailable.RemoveAt(randomIndex);
+                }
+
+            }
+
+
+
+        }
+
+        else if (random == 2)
+        {
+            _roundState = RoundState.TrioRound;
+            GameObject playerMostPoint = null;
+            foreach (GameObject player in _players)
+            {
+                if(playerMostPoint == null)
+                {
+                    playerMostPoint = player;
+                    
+                }
+                /*else if(player._point > playerMostPoint._point)
+                {
+                    playerMostPoint.tag = "team2";
+                    _team2.Add(playerMostPoint);
+
+                    playerMostPoint = player;
+                }*/
+                else
+                {
+                    player.tag = "team2";
+                    _team2.Add(player);
+                }
+            }
+            playerMostPoint.tag = "team1";
+            _team1.Add(playerMostPoint);
+
+
+
+
+        }
+    }
+}
--- Assets/Scripts/RoundManage/Timer.cs
+++ Assets/Scripts/RoundManage/Timer.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
@@ -10,31 +9,21 @@ public class Timer : MonoBehaviour
     private bool _isTimerRunning = false;
     public TextMeshProUGUI _timerText;
 
-    private void Start()
-    {
-        // Démarrez le timer au début (peut également être déclenché par un événement ou une action)
-        StartTimer();
-    }
+
     private void Update()
     {
-        // Vérifiez si le timer est en cours d'exécution
         if (_isTimerRunning)
         {
-            // Réduire le temps restant
             _countdownTime -= Time.deltaTime;
-            // Vérifiez si le temps est écoulé
             if (_countdownTime <= 0)
             {
-                // Le temps est écoulé, exécutez votre action ici (par exemple, affichez un message)
                 Debug.Log("Temps écoulé !");
-                // Arrêtez le timer
                 _timerText.text = "0:0";
 
                 StopTimer();
             }
             else
             {
-                // Calcul des minutes et des secondes restantes
                 int minutes = Mathf.FloorToInt(_countdownTime / 60);
                 int seconds = Mathf.FloorToInt(_countdownTime % 60);
                 _timerText.text = minutes + ":" + seconds;
@@ -46,7 +35,6 @@ public class Timer : MonoBehaviour
         _isTimerRunning = true;
     }
 
-    // Arrêtez le timer
     public void StopTimer()
     {
         _isTimerRunning = false;
