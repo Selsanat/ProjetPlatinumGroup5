61af016801129b8b7920b6059587fad1f36b0dea
Selsanat <leandro.benites@edu.devinci.fr>
2023-10-23 16:42:43 +0200

    CharacterController ball

---
+++ Assets/Scripts/RoundManager/GameStateTemplate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameStateTemplate
+{
+    public GameStateMachine StateMachine { get; private set; }
+
+    
+    protected void ChangeState(GameStateTemplate state) => StateMachine.ChangeState(state);
+
+    public void Init(GameStateMachine stateMachine)
+    {
+        StateMachine = stateMachine;
+        OnStateInit();
+    }
+
+    public void StateEnter(GameStateTemplate previousState) => OnStateEnter(previousState);
+    public void StateExit(GameStateTemplate nextState) => OnStateExit(nextState);
+
+    public void StateUpdate() => OnStateUpdate();
+    protected virtual void OnStateInit() { }
+    protected virtual void OnStateEnter(GameStateTemplate previousState) { }
+    protected virtual void OnStateExit(GameStateTemplate nextState) { }
+    protected virtual void OnStateUpdate() { }
+}
