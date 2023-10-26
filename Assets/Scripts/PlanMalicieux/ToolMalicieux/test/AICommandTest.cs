00a7d8b61efe0ffc0738d9c2651d1e576cb1c26f
Julien <blondelot.julien@gmail.com>
2023-10-26 14:32:13 +0200

    Map 1 V3 (Map finit sans asset)

---
+++ Assets/Scripts/PlanMalicieux/ToolMalicieux/test/AICommandTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("B"))
+        {
+            other.gameObject.transform.position = new Vector3(
+                other.gameObject.transform.position.x,
+                other.gameObject.transform.position.y - 1,
+                other.gameObject.transform.position.z
+            );
+        }
+    }
+}
