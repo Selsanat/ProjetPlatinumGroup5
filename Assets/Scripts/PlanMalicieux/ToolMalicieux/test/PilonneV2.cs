ba6f14aaad2bc9863c41a1b2970d58084bfefffc
Rouxcoooooooool <mulalictimothe78@gmail.com>
2023-10-30 14:15:08 +0100

    Update PilonneV2.cs

--- Assets/Scripts/PlanMalicieux/ToolMalicieux/test/PilonneV2.cs
+++ Assets/Scripts/PlanMalicieux/ToolMalicieux/test/PilonneV2.cs
@@ -33,10 +33,6 @@ public class PilloneV2 : MonoBehaviour
     {
         if (!isMoving)
             return;
-            if (collision.CompareTag("Player"))
-        {
-            
-        }
 
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, currentPillar.position, step);
