02c74413c30708d1d9cfd860a5a14a0c7752611b
Selsanat <leandro.benites@edu.devinci.fr>
2023-11-01 20:50:11 +0100

    WIP menu Selection Perso, slider fait

--- Assets/Scripts/UiSelectionPerso/CharacterSelector.cs
+++ Assets/Scripts/UiSelectionPerso/CharacterSelector.cs
@@ -9,29 +9,29 @@ public class CharacterSelector : MonoBehaviour
     UnityEngine.InputSystem.PlayerInput playerInputs;
     private Vector2 input;
     private int index = 0;
-    private float PaddingLeft = 0;
-    HorizontalLayoutGroup horizontalLayoutGroup;
+    public float PaddingLeft = 0;
+    public HorizontalLayoutGroup horizontalLayoutGroup;
 
     void Awake()
     {
         Canvas canvas = ManagerManager.Instance.selectionPersoCanvas;
         transform.SetParent(canvas.transform);
         playerInputs = GetComponent<UnityEngine.InputSystem.PlayerInput>();
-        horizontalLayoutGroup = GetComponentInParent<HorizontalLayoutGroup>();
+        horizontalLayoutGroup = GetComponentInChildren<HorizontalLayoutGroup>();
     }
 
     void Start()
     {
-        //ManagerManager.Instance.
         transform.GetComponentInChildren<Button>().Select();
-        playerInputs.actions.actionMaps[1].actions[2].performed += ctx => SwipeRight();
-        playerInputs.actions.actionMaps[1].actions[3].performed += ctx => SwipeLeft();
+        playerInputs.actions.actionMaps[1].actions[2].started += ctx => SwipeRight();
+        playerInputs.actions.actionMaps[1].actions[3].started += ctx => SwipeLeft();
     }
 
     void Update()
     {
         input = playerInputs.actions.actionMaps[0].actions[0].ReadValue<Vector2>();
         horizontalLayoutGroup.padding.left = (int)PaddingLeft;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(horizontalLayoutGroup.GetComponent<RectTransform>());
     }
 
     void SwipeRight()
