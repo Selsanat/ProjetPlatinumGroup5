bcf8b21db920a29201f5492236ed47f23ab0ef8f
clément bostyn <clemclem20002@gmail.com>
2023-11-03 14:25:08 +0100

    ajout couleur et parametre boule

--- Assets/Scripts/Player/Scriptables/BouleParams.cs
+++ Assets/Scripts/Player/Scriptables/BouleParams.cs
@@ -21,15 +21,18 @@ public class BouleParams : ScriptableObject
 
     [Header("vitesse � laquelle la boule va revenir en place apr�s un bug de d�placement")]
     [Space(5)]
+    [Range(0, 9999)]
     public float _resetSpeed = 400;
     [Space(10)]
 
     [Header("Vitesse de lancer de la boule")]
     [Space(5)]
+    [Range(0, 9999)]
     public float _speedThrowing = 10.0f;
     [Space(10)]
 
     [Header("Vitesse du lancer de la boule")]
+    [Range(0, 1000)]
     public float _speedBack = 2.0f;
     [Space(10)]
 
@@ -56,6 +59,25 @@ public class BouleParams : ScriptableObject
     [Header("Distance entre la boule et le player ")]
     [Space(5)]
     public float _size = 0;
-     
+    [Space(15)]
+
+    [Header("Les distances des boules")]
+    [Space(10)]
+    [Header("la distances acceptable lorsque la boule reviens et va rebondire sur les endroits sur lesquelles elle avait rebondi")]
+    [Space(5)]
+    [Range (0, 50)]
+    public float _distancePoints = 0.5f;
+    [Space(15)]
+    [Header("la distances acceptable lorsque la boule est assez proche dans l'update (ca check au cas ou la boule est coicé sur une platform)")]
+    [Space(5)]
+    [Range(0, 5)]
+    public float _distanceCloseEneaughtUpdate = 0.25f;
+    [Space(15)]
+    [Header("la distances non-acceptable lorsque la boule est trop loin dans l'update (ca check au cas ou la boule est coicé sur une platform)")]
+    [Space(5)]
+    [Range(0, 6)]
+    public float _distanceTooFarUpdate = 0.5f;
+
+
 }
 
